2f1afff57455f7e8be8c8a171f7d960dd1a9be2a
Aldo Ayala <ayalaa63@gmail.com>
2018-02-20 15:41:43 -0800

    First commit

---
+++ thingsToDo/AddItemTVC.swift
@@ -0,0 +1,115 @@
+//
+//  AddItemTVC.swift
+//  thingsToDo
+//
+//  Created by Aldo Ayala on 2/20/18.
+//  Copyright © 2018 Aldo Ayala. All rights reserved.
+//
+
+import UIKit
+
+class AddItemTVC: UITableViewController {
+
+    var listOfChores: [String] = []
+    
+    @IBAction func addItem(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add Chore", message: "Name of Chore", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Save", style: .default)
+            let choreName = alert.textFields
+            print(choreName)
+           // listOfChores.append(choreName)
+        //print(listOfChores)
+            
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+        
+        alert.addTextField { (textField: UITextField) in
+            textField.keyboardAppearance = .dark
+            textField.keyboardType = .emailAddress
+            textField.autocorrectionType = .no
+            textField.placeholder = "Enter Chore"
+            textField.textColor = UIColor.black
+            textField.font = UIFont(name: "Helvetica", size: 18)
+        }
+        
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return listOfChores.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "choreCell", for: indexPath)
+
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ thingsToDo/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  thingsToDo
+//
+//  Created by Aldo Ayala on 2/6/18.
+//  Copyright © 2018 Aldo Ayala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var nameOfTask = [String]()
+    var nameOFPerson = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func saveButton(_ sender: Any) {
+        
+    }
+    
+}
+
